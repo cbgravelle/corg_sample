23e848c86a72a85731c7b229187fe1b5ba344617
Corey Gravelle <cbgravelle@gmail.com>
2013-08-30 09:30:25 -0700

    Initial blueprint push

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,11 @@ describe PagesController do
 
   render_views
 
+  before (:each) do
+      @base_title = "Corg-Sample"
+  end
+
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -13,7 +18,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-        :content => "Corg-Sample | Home")
+        :content => "#{@base_title} | Home")
     end
 
     it "should have a non-blank body" do
@@ -30,7 +35,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-        :content => "Corg-Sample | Contact")
+        :content => "#{@base_title} | Contact")
     end
   end
 
@@ -42,7 +47,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-        :content => "Corg-Sample | About")
+        :content => "#{@base_title} | About")
     end
   end
 
