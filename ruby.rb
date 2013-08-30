fc18c7849098dabb23e80742ffe14398501f42a6
Corey Gravelle <cbgravelle@gmail.com>
2013-08-30 12:50:30 -0700

    added custom.css

---
+++ ruby.rb
@@ -0,0 +1,15 @@
+class User
+	
+	# setting 'getter' and 'setter' for defining attributes.
+	attr_accessor :name, :email
+
+	def initialize(attributes = {})
+		@name = attributes[:name]
+		@email = attributes[:email]
+	end
+
+	def formatted_email
+		"#{@name} <#{@email}"
+	end
+
+end
