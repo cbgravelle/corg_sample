3f329d503a8dad2fbd778ff1bdadb592f9866693
Corey Gravelle <cbgravelle@gmail.com>
2013-09-04 14:39:12 -0700

    added gravatar and subseq helper for user show page

--- Gemfile
+++ Gemfile
@@ -29,8 +29,9 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-# gem 'strong_parameters'
 gem 'protected_attributes'
+
+gem 'gravatar_image_tag', '0.1.0'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -22,6 +22,21 @@ describe UsersController do
       assigns(:user).should == @user
     end
 
+    it "should have the right title" do
+      get :show, "id" => @user.id
+      response.should have_selector('title', :content => @user.name)
+    end
+
+    it "should have the user's name" do
+      get :show, "id" => @user.id
+      response.should have_selector('h2', :content => @user.name)        
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector('h2>img', :class => "gravatar")
+    end
+
   end
 
   describe "GET 'new'" do
