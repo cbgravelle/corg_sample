cb08a97e7c27355f88e399335754908d2729cd08
Corey Gravelle <cbgravelle@gmail.com>
2013-09-04 13:37:58 -0700

    finished password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,38 @@ class User < ActiveRecord::Base
 							# Rails automatically creates a 'password_confirmation' attr with:
 							:confirmation 	=> true,  	
 							:length 		=> {:within => 6..40}
+
+	before_save :encrypt_password
+
+	def has_password?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+
+	class << self
+		def authenticate(email, submitted_password)
+			#inside a class method, "User." is unnecessary:
+			user = User.find_by_email(email)
+			return nil if user.nil?
+			return user if user.has_password?(submitted_password)
+		end
+	end
+
+	private 	#'private' methods are not accessable outside of model 
+
+		def encrypt_password
+			self.salt = make_salt if self.new_record?
+			self.encrypted_password = encrypt(self.password)
+		end
+
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
+
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
 end
